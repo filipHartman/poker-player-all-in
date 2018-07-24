34066e353d6a779ca3f40991de2ad516173c86de
Miklos Beothy <beothy.miklos@gmail.com>
2018-07-24 10:41:46 +0200

    initial commit

---
+++ Player.js
@@ -0,0 +1,15 @@
+class Player {
+  static get VERSION() {
+    return '0.1';
+  }
+
+  static betRequest(gameState, bet) {
+    
+    bet(0);
+  }
+
+  static showdown(gameState) {
+  }
+}
+
+module.exports = Player;
